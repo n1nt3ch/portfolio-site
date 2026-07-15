d462fcc9139e4a0a56f9fd488fde1190aa398bf8
Касимов Ильшат Адгамович <KasimovIA@tatenergosbyt.corp>
2026-07-15 16:53:05 +0300

    fix: add CSS module declarations for TypeScript

---
+++ src/vite-env.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="vite/client" />
+
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.less' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
